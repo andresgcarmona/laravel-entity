d208416f6b160ea61c4e39f1dcf180edd147e35c
Andres G. Carmona <ing.andres.carmona@gmail.com>
2019-08-18 22:15:33 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Laravel Entity.
+
+A Laravel package to give your PHP standard classes identity and extra functionality on the fly.
+
+- [Laravel Repository on Packagist](https://packagist.org/packages/polaris/laravel-entity)
+- [Laravel Repository on GitHub](https://github.com**/andresgcarmona/laravel-entity)
---
+++ src/LaravelEntityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Polaris\Providers;
+
+    use Illuminate\Support\ServiceProvider;
+
+    class LaravelEntityServiceProvider extends ServiceProvider
+    {
+        /**
+         * Register services.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
+
+        /**
+         * Bootstrap services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            //
+        }
+    }
